a4f97396a4d27f15dff5d3cdcda9d0f2e11d5a33
Patrick <pateast678@gmail.com>
2020-02-19 16:04:01 -0500

    Good place to save before integrating jquery into react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import Searchbar from './components/Searchbar';
+
 function App() {
   return (
     <div className="App">
       <div className="App-header">
-        f
+        <Searchbar />
       </div>
     </div>
   );
---
+++ src/components/Searchbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Searchbar extends React.Component {
+    render() {
+        return (
+            <div>
+                <input className="searchbox"></input>
+            </div>
+        )
+    }
+}
